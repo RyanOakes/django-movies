a43c2cd2b6bf80edff425be99122f9ee45a4525e
oakes <oakesonline@gmail.com>
2016-05-04 21:57:50 -0400

    Updates

--- movieratings/movielens/models.py
+++ movieratings/movielens/models.py
@@ -1,23 +1,20 @@
 from django.db import models
 
 
+class Rater(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=2)
+    occupation = models.CharField(max_length=20)
+    zip_code = models.Charfield(max_length=10)
+
+
 class Movie(models.Model):
     movie_id = models.IntegerField()
     title = models.CharField(max_length=200)
 
 
-class Rater(models.Model):
-    ratings = models.ForeignKey(Ratings, on_delete=models.CASCADE)
-    user_id = models.IntegerField()
-    age = models.IntegerField()
-    gender = models.CharField(max_length=1)
-    occupation = models.CharField(max_length=20)
-    zip_code = models.IntegerField(dfault=0)
-
-
 class Ratings(models.Model):
     rater = models.ForeignKey(Rater, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user_id = models.IntegerField()
-    movie_id = models.IntegerField()
     rating = models.IntegerField()
