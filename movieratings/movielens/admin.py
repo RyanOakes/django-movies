c538dbf086409de3da344ca03471404a6345ce79
oakes <oakesonline@gmail.com>
2016-05-05 08:40:25 -0400

    Models fix and admin updates

--- movieratings/movielens/admin.py
+++ movieratings/movielens/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie, Rater, Ratings
+
+admin.site.register(Movie)
+admin.site.register(Rater)
+admin.site.register(Ratings)
 
 # Register your models here.
